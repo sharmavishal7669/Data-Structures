9c3ee2107e34556888650e9820ea5e32aa9d1d21
Vishal Sharma <vsunite3@gmail.com>
2021-02-21 10:54:44 +0530

    Added Singly Linked List method - insertAtPos()

--- src/linkedList/singlyLinkedList/Main.java
+++ src/linkedList/singlyLinkedList/Main.java
@@ -15,5 +15,18 @@ public class Main {
         sll.insertAtLast(50);
         sll.display();
         sll.getLength();
+        sll.insertAtPos(3,60);
+        sll.display();
+        sll.insertAtPos(2,70);
+        sll.display();
+        sll.insertAtPos(7,80);
+        sll.display();
+        sll.getLength();
+        sll.insertAtPos(1,90);
+        sll.display();
+        sll.insertAtPos(10,100);
+        sll.display();
+        sll.insertAtPos(12,110);
+        sll.getLength();
     }
 }
--- src/linkedList/singlyLinkedList/SinglyLinkedList.java
+++ src/linkedList/singlyLinkedList/SinglyLinkedList.java
@@ -47,6 +47,43 @@ public class SinglyLinkedList {
         length++;
     }
 
+    public void insertAtPos(int pos, int data)
+    {
+        if(pos < 1 || pos >length+1)
+        {
+            System.out.println("Oops! You're trying to insert at invalid position. Please review your list below and try again.");
+            display();
+        }
+        else if(pos == 1)
+        {
+            insertAtFirst(data);
+        }
+        else if(pos == length+1)
+        {
+            insertAtLast(data);
+        }
+        else
+        {
+            ListNode newNode = new ListNode(data);
+            ListNode current = head;
+            ListNode currentNext = current.next;
+
+            int pointer = 1;
+            while(pointer < pos-1)
+            {
+                current = current.next;
+                currentNext = current.next;
+                pointer++;
+            }
+
+
+            current.next = newNode;
+            newNode.next = currentNext;
+
+            length++;
+        }
+    }
+
     public void getLength()
     {
         System.out.println("Length : "+length);
